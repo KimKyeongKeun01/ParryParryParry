498a7c2d6081e095ffe60a40ee4d6a20acc756cb
경근 <honne.kim01@gmail.com>
2026-04-06 06:26:15 +0900

    [Update] 우탄 인트로 완료

--- Assets/02Script/Enemy/Boss/Utan/BossStage_Utan.cs
+++ Assets/02Script/Enemy/Boss/Utan/BossStage_Utan.cs
@@ -2,10 +2,37 @@ using UnityEngine;
 
 public class BossStage_Utan : BossStage
 {
-    [SerializeField] private CameraShakeProfile growlingProfile;
+    [SerializeField] private CameraShakeProfile landingProfile;
+    [SerializeField] private CameraShakeProfile cryingProfile;
 
-    public void S_GrowlingShake()
+    private void Update()
     {
-        CameraManager.Instance.Shake.Play(growlingProfile);
+        // 테스트
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            S_CryingShake();
+        }
+        else if (Input.GetKeyDown(KeyCode.J))
+        {
+            S_LandingShake();
+        }
+    }
+
+    public void S_LandingShake()
+    {
+        Debug.Log("[Boss Stage] Landing Shake");
+        Debug.Log($"[Signal Shake] obj={name}, id={GetInstanceID()}, frame={Time.frameCount}");
+        Debug.Log($"[Signal Shake] mainCam={Camera.main?.name}");
+
+        CameraManager.Instance.Shake.Play(landingProfile, 3);
+    }
+
+    public void S_CryingShake()
+    {
+        Debug.Log("[Boss Stage] Crying Shake");
+        Debug.Log($"[Signal Shake] obj={name}, id={GetInstanceID()}, frame={Time.frameCount}");
+        Debug.Log($"[Signal Shake] mainCam={Camera.main?.name}");
+
+        CameraManager.Instance.Shake.Play(cryingProfile, Vector2.left);
     }
 }
